c38aa2eeecc3da2767a6195556026a39e92c0107
PalmPalmPalmPalmPalm <chitipat.c2548@gmail.com>
2026-02-08 19:09:40 +0700

    Hello Window!

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const int SCN_WIDTH = 800;
+const int SCN_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void inputProcess(GLFWwindow *window);
+
+int main() {
+    // glfw: init config
+    // -----------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // glfw: create window
+    // -------------------
+    GLFWwindow *window =
+        glfwCreateWindow(SCN_WIDTH, SCN_HEIGHT, "Hello Window", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: Load pointer manager
+    // --------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // glfw: render loop
+    // -------------------
+    while (!glfwWindowShouldClose(window)) {
+        inputProcess(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void inputProcess(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
